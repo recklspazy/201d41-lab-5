a335b1dc7cfd0d045f7aa395f8db6c2a6180f428
Jung H Davis <cy4jcd@yahoo.com>
2018-10-26 18:29:06 -0700

    update on test

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-  return [a + b, ' The multiplication of ' + a + ' and ' + b + ' is ' + (a *b) + '.'];
+  return [a * b, ' The multiplication of ' + a + ' and ' + b + ' is ' + (a *b) + '.'];
 
 }
 
@@ -39,10 +39,14 @@ function multiply(a, b) { //eslint-disable-line
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array
+ where the first element is the sum of those three numbers, the second element is the product of those three numbers,
+   and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input
+    into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
+16 " The multiplication of 4 and 7 is 28." "4 and 7 and 5 sums to 16" " The product of 4 and 7 and 5 is undefined"
 
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 
@@ -50,18 +54,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  return [a+b+c, ' The sum of ' + a + ' and ' + b + ' multiplied by ' + c + ' is ' + (a+b*c) + '.'];
-
+  return [sum(sum(a,b)[0],c)[0], multiply(multiply(a,b)[0], c)[0], a+ ' and ' +b+ ' and ' +c+ ' sum to ' + sum(sum(a,b)[0],c)[0] + '.', 'The product of ' +a+ ' and ' +b+ ' and ' +c+ ' is ' + multiply(multiply(a,b)[0],c)[0] + '.'];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+//testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the
+ first element is the sum of the numbers in the array,
+  and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
@@ -70,10 +75,12 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-var testArray = [2, 3, 4]; //eslint-disable-line
+//var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  var testArray = [2,3,4];
+  return [testArray[0], testArray [1], testArray [2],' were numbers in array. so ' + testArray[0] + ' and ' + testArray[1] + ' and ' + testArray[2] + ' is ' +
+  (testArray[0] + testArray[1] + testArray[2]) +'.'];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
